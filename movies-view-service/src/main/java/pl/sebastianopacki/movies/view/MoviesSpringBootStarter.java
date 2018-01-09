5cc7a20677d8f89dae546777f5fdca90bdbdc1c4
sopacki <opax92@gmail.com>
2018-01-09 16:20:44 +0100

    add configuration to started web app with tomcat

--- movies-view-service/src/main/java/pl/sebastianopacki/movies/view/MoviesSpringBootStarter.java
+++ movies-view-service/src/main/java/pl/sebastianopacki/movies/view/MoviesSpringBootStarter.java
@@ -2,6 +2,8 @@ package pl.sebastianopacki.movies.view;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -9,8 +11,13 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan("pl.sebastianopacki")
-public class MoviesSpringBootStarter {
+public class MoviesSpringBootStarter extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(MoviesSpringBootStarter.class);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MoviesSpringBootStarter.class);
     }
 }
