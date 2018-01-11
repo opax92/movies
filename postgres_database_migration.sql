af91f86a7f6390ae0f7e77551dd4585354ceee90
Sebastian Opacki <opax92@users.noreply.github.com>
2018-01-11 11:21:25 +0100

    Update postgres_database_migration.sql

--- postgres_database_migration.sql
+++ postgres_database_migration.sql
@@ -1,36 +1,27 @@
 DROP SEQUENCE IF EXISTS hibernate_sequence;
 
-CREATE SEQUENCE hibernate_sequence
-       INCREMENT BY 1
-       MINVALUE 1
-       CACHE 1
-       NO CYCLE;
+CREATE SEQUENCE hibernate_sequence INCREMENT BY 1 MINVALUE 1 CACHE 1 NO CYCLE;
 
 DROP TABLE IF EXISTS movie_actors CASCADE;
 
-CREATE TABLE movie_actors
+CREATE TABLE movie_actors 
 (
-   movie_id  integer        NOT NULL,
-   actors    text
+  movie_id   INTEGER NOT NULL,
+  actors     text
 );
 
 DROP TABLE IF EXISTS movie CASCADE;
 
-CREATE TABLE movie
+CREATE TABLE movie 
 (
-   id         integer     NOT NULL,
-   director   text,
-   rate       float8,
-   title      text,
-   createdat  timestamp
+  id          INTEGER NOT NULL,
+  director    text,
+  rating      float8,
+  title       text,
+  createdat   TIMESTAMP
 );
 
-ALTER TABLE public.movie
-   ADD CONSTRAINT movie_tbl_pkey
-   PRIMARY KEY (id);
+ALTER TABLE movie ADD CONSTRAINT movie_tbl_pkey PRIMARY KEY (id);
 
-ALTER TABLE movie_actors
-  ADD CONSTRAINT movie_id FOREIGN KEY (movie_id)
-  REFERENCES movie (id)
-  ON UPDATE NO ACTION
-  ON DELETE NO ACTION;
+ALTER TABLE movie_actors ADD CONSTRAINT movie_id FOREIGN KEY (movie_id) REFERENCES movie (id) ON UPDATE NO ACTION ON DELETE NO ACTION;
+
